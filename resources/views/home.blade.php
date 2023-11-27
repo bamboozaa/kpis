26cc3fba2bffaea4b5cd3e8aa3e540c8f0fa782e
bamboozaa <chukadee@gmail.com>
2023-11-27 16:57:50 +0700

    kpis commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,27 @@
                     <div class="card-header">{{ __('Dashboard') }}</div>
 
                     <div class="card-body">
-                        @if (session('status'))
+                        {{-- @if (session('status'))
                             <div class="alert alert-success" role="alert">
                                 {{ session('status') }}
                             </div>
-                        @endif
+                        @endif --}}
+
+                        @php
+                            $current_year = \Carbon\Carbon::now()->year;
+                            $startDate = $current_year . '-08-01';
+                            $endDate = $current_year + 1 . '-07-31';
+                            $current_date = \Carbon\Carbon::now();
+
+                            if ($current_date >= $startDate && $current_date <= $endDate) {
+                                $current_year2 = \Carbon\Carbon::parse($current_date)->format('Y');
+                            }
+                        @endphp
 
                         <div class="row text-center">
                             <h1>{{ __('PERFORMANCE REVIEW FORM') }}</h1>
                             <p>{{ __('(แบบประเมินผลการปฏิบัติงาน)') }}</p>
+                            <p>{{ __('ประจำปีการศึกษา') . $current_year2 }}</p>
                         </div>
                     </div>
                 </div>
