f6f18eef5500f0b7e2417373f22ee7691bb61a44
bamboozaa <chukadee@gmail.com>
2023-11-28 16:32:30 +0700

    kpis commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,21 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $users = User::all();
-        return view('home', compact('users'));
-    }
-
-    public function getDepartment($department) {
-        $dep_full_name = trim($department);
-        $left_pos = strripos($dep_full_name, "(") + 1;
-        $dep_full_name = substr($dep_full_name, $left_pos, strlen($dep_full_name));
-        $dep_full_name = substr($dep_full_name, 0, strlen($dep_full_name) - 1);
-        $dep_full_name = trim($dep_full_name);
-        return $dep_full_name;
-    }
-
-    public function getOwnerUnitName($id) {
-        $owner_name = App\Models\Unit::findOrFail($id);
-        return $owner_name;
+        return view('home');
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,14 +4,14 @@
 
     @php
 
-    function getDepartment($department) {
-        $dep_full_name = trim($department);
-        $left_pos = strripos($dep_full_name, "(") + 1;
-        $dep_full_name = substr($dep_full_name, $left_pos, strlen($dep_full_name));
-        $dep_full_name = substr($dep_full_name, 0, strlen($dep_full_name) - 1);
-        $dep_full_name = trim($dep_full_name);
-        return $dep_full_name;
-    }
+    // function getDepartment($department) {
+    //     $dep_full_name = trim($department);
+    //     $left_pos = strripos($dep_full_name, "(") + 1;
+    //     $dep_full_name = substr($dep_full_name, $left_pos, strlen($dep_full_name));
+    //     $dep_full_name = substr($dep_full_name, 0, strlen($dep_full_name) - 1);
+    //     $dep_full_name = trim($dep_full_name);
+    //     return $dep_full_name;
+    // }
 
     function getOwnerUnitName($id) {
         $owner_name = App\Models\Unit::findOrFail($id);
@@ -65,7 +65,7 @@
                                     <tr>
                                         <th scope="col" class="text-center text-nowrap"
                                             style="background-color: #f3f3f3">หน่วยงาน :</th>
-                                        <td>{{ getDepartment(Auth::user()->department) }}</td>
+                                        <td>{{ Auth::user()->department }}</td>
                                     </tr>
                                     <tr>
                                         <th scope="col" class="text-center text-nowrap"
