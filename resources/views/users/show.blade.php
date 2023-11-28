a71f66078130f478b5888fe27bd4e095e85a1464
bamboozaa <chukadee@gmail.com>
2023-11-28 16:41:46 +0700

    kpis

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -19,24 +19,6 @@
 @stop
 
 @section('content')
-    @php
-
-        function getDepartment($department)
-        {
-            $dep_full_name = trim($department);
-            $left_pos = strripos($dep_full_name, '(') + 1;
-            $dep_full_name = substr($dep_full_name, $left_pos, strlen($dep_full_name));
-            $dep_full_name = substr($dep_full_name, 0, strlen($dep_full_name) - 1);
-            $dep_full_name = trim($dep_full_name);
-            return $dep_full_name;
-        }
-
-        function getOwnerUnitName($id)
-        {
-            $owner_name = App\Models\Unit::findOrFail($id);
-            return $owner_name;
-        }
-    @endphp
 
     <div class="container">
         <div class="row justify-content-center">
@@ -73,7 +55,7 @@
                         <div class="mb-3">
                             <label for="department" class="form-label">หน่วยงาน :</label>
                             <input type="text" class="form-control" id="department"
-                                value="{{ getDepartment($user->department) }}" readonly>
+                                value="{{ $user->department }}" readonly>
                         </div>
 
                         <div class="mb-3">
