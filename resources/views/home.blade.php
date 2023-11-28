0970241884b26e93b5c5f4033a97c319f8013df5
bamboozaa <chukadee@gmail.com>
2023-11-28 16:35:18 +0700

    kpis commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,6 @@
 
 @section('content')
 
-    @php
-
-    // function getDepartment($department) {
-    //     $dep_full_name = trim($department);
-    //     $left_pos = strripos($dep_full_name, "(") + 1;
-    //     $dep_full_name = substr($dep_full_name, $left_pos, strlen($dep_full_name));
-    //     $dep_full_name = substr($dep_full_name, 0, strlen($dep_full_name) - 1);
-    //     $dep_full_name = trim($dep_full_name);
-    //     return $dep_full_name;
-    // }
-
-    function getOwnerUnitName($id) {
-        $owner_name = App\Models\Unit::findOrFail($id);
-        return $owner_name;
-    }
-    @endphp
-
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -70,7 +53,7 @@
                                     <tr>
                                         <th scope="col" class="text-center text-nowrap"
                                             style="background-color: #f3f3f3">ชื่อผู้บังคับบัญชาโดยตรง :</th>
-                                        <td>{{ getOwnerUnitName(Auth::user()->unit_id)->user_name['fullname'] }}</td>
+                                        <td>{{ Auth::user()->unit_id->user_name['fullname'] }}</td>
                                     </tr>
                                 </table>
                             </div>
