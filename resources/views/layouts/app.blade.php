32dbdca65a0cae359129f17d712b1217041f04c0
bamboozaa <chukadee@gmail.com>
2023-12-13 11:50:30 +0700

    kpis commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,8 +31,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
+
                 <a class="navbar-brand" href="{{ url('/home') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src="{{ URL::asset('images/logo-utcc60th.png') }}" alt="" width="60" height="54" class="d-inline-block align-text-middle">
+                    {{ __('KPIs') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
