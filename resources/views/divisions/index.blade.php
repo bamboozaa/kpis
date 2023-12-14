2cfa9a4151d5a00e868e8d728d9165ccbc1b3e30
bamboozaa <chukadee@gmail.com>
2023-12-14 14:37:26 +0700

    kpis commit

--- resources/views/divisions/index.blade.php
+++ resources/views/divisions/index.blade.php
@@ -36,48 +36,50 @@
                 </nav>
             </div>
             <div class="col-md-3" style="text-align: right!important;">
-                <a href="{{ route('divisions.create') }}" class="btn btn-primary mb-2"><i class="bi bi-plus-square"></i>
-                    Create New Division</a>
+                <a href="{{ route('divisions.create') }}" class="btn btn-primary mb-2">
+                    <i class="bi bi-plus-square fs-sm"></i>
+                    <span class="ms-2">{{ __('Create New Division') }}</span>
+                </a>
             </div>
         </div>
         <div class="row justify-content-end">
 
 
-                <div class="card">
-                    <div class="card-header">{{ __('All Divisions') }}</div>
+            <div class="card">
+                <div class="card-header">{{ __('All Divisions') }}</div>
 
-                    <div class="card-body">
-                        <div class="table-responsive mt-3">
-                            <table class="table table-hover">
-                                <thead>
-                                    <tr>
-                                        <th scope="col" class="text-center text-nowrap">Cost Center</th>
-                                        <th scope="col" class="text-nowrap">ชื่อฝ่าย</th>
-                                        {{-- <th scope="col" class="text-center">Actions</th> --}}
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @if (count($divisions) > 0)
-                                        @foreach ($divisions as $division)
-                                            <tr>
-                                                <td class="text-center">{{ $division->cost_center }}</td>
-                                                <td>{{ $division->div_name }}</td>
-                                                {{-- <td class="text-center text-nowrap">
+                <div class="card-body">
+                    <div class="table-responsive mt-3">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col" class="text-center text-nowrap">Cost Center</th>
+                                    <th scope="col" class="text-nowrap">ชื่อฝ่าย</th>
+                                    {{-- <th scope="col" class="text-center">Actions</th> --}}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if (count($divisions) > 0)
+                                    @foreach ($divisions as $division)
+                                        <tr>
+                                            <td class="text-center">{{ $division->cost_center }}</td>
+                                            <td>{{ $division->div_name }}</td>
+                                            {{-- <td class="text-center text-nowrap">
                                                     <a href="{{ route('divisions.edit', $division->div_id) }}" class="btn btn-warning btn-sm">Edit</a>
                                                 </td> --}}
-                                            </tr>
-                                        @endforeach
-                                    @else
+                                        </tr>
+                                    @endforeach
+                                @else
                                     <tr>
                                         <td colspan="2">ไม่พบข้อมูลที่ท่านต้องการค้นหา</td>
                                     </tr>
-                                    @endif
+                                @endif
 
-                                </tbody>
-                            </table>
-                        </div>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
+            </div>
 
         </div>
     </div>
